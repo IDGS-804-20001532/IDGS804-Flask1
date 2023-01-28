8977261946d95377b2d3c082c30d8826b4e4c5e6
Adrian Hernandez <hernandezsanchezluisadrian1@gmail.com>
2023-01-27 20:13:39 -0600

    Tema Flask1, segunda clase 27/01/2023

--- tema3-main.py
+++ tema3-main.py
@@ -1,27 +1,28 @@
+
 from flask import Flask
 
-app=Flask(__name__)
+app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Hola mundo"
+    return "Hola Mundo!"
 
-#Parametros String 
 @app.route("/user/<string:user>")
 def user(user):
-    return "Hola "+ user
+    return "Hola".format(user)
 
-#Parametros int 
-@app.route("/numero/<int:n>")
-def user(n):
-    return "Número {}".format(n)
+@app.route("/edad/<int:n>")
+def edad(n):
+    return "Número".format(n)
 
-#Mas de un parametro 
-@app.route("/user/<int:id>/<string:username>")
-def user(id, username):
-    return "Id: {} nombre: {}".format(id,username)
+@app.route("/username/<int:id>/<string:username>")
+def username(id, username):
+    return "Nombre: {0} Id: {1}!".format(username,id)
+
+@app.route("/suma/<float:num1>/<float:num2>")
+def suma(num1, num2):
+    return "La suma de {0} y {1} es  {2}".format(num1, num2, num1 + num2)
 
 
-
-if __name__=="__main__":
-    app.run(debug=True,port=3000)
+if __name__== "__main__":
+    app.run(debug = True,port=3000)
